f8db03215d8feb5d47ed8da04ab71038277b2e18
ubaid <ubaidsaleem825@gmail.com>
2025-04-24 10:59:10 +0500

    Add commit into code

--- qr_code.py
+++ qr_code.py
@@ -1,13 +1,18 @@
 # import library
 import qrcode
 import qrcode.constants
-
+# Set variable of QR code
 qr_code = qrcode.QRCode(version=1,
                         error_correction=qrcode.constants.ERROR_CORRECT_H,
                         box_size=12,
                         border=2)
 
+# Add path to which link
 qr_code.add_data("repo")
 qr_code.make(fit=True)
-image = qr_code.make_image(fill_color='black',light="lightblue", back_color='white')
+
+# Change background color and other aspect
+image = qr_code.make_image(fill_color='black', back_color='white')
+
+# save QR Code into image
 image.save("github_repo_qrcode.png")
